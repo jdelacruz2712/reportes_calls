b670b64a7ab7397db40574cbfec8334596f489c3
Alan Cornejo <alancornejo@icloud.com>
2017-02-06 19:38:50 -0500

    Eliminar Seeder innecesarios

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
         $this->call(EstadosTableSeeder::class);
         $this->call(EstadosVisiblesTableSeeder::class);
         $this->call(EstadosCallsTableSeeder::class);
