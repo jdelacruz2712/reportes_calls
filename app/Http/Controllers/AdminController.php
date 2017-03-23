8c588b311b03c645ed32b824398e9cc0bb5cc757
Raúl Dominguez <jdominguezr93@gmail.com>
2017-03-23 13:26:55 -0500

    Correcion de la validaccion que define en que momento se debe mostrar la ventana de cambiar contraseña.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace Cosapi\Http\Controllers;
 
 use Cosapi\Models\Anexo;
+use Cosapi\Models\User;
 use Illuminate\Http\Request;
 use Cosapi\Http\Requests;
 use Illuminate\Support\Facades\Auth;
@@ -21,33 +22,30 @@ class AdminController extends Controller
     public function __construct(){
 
         if (!Session::has('UserId')) {
-            Session::put('UserId'   ,Auth::user()->id   );
+            Session::put('UserId'       ,Auth::user()->id   );
         }
 
         if (!Session::has('UserRole')) {
-            Session::put('UserRole'   ,Auth::user()->role   );
+            Session::put('UserRole'     ,Auth::user()->role   );
         }
 
         if (!Session::has('UserName')) {
-            Session::put('UserName'   ,Auth::user()->primer_nombre.' '.Auth::user()->apellido_paterno );
+            Session::put('UserName'     ,Auth::user()->primer_nombre.' '.Auth::user()->apellido_paterno );
         }
 
         if (!Session::has('UserSystem')) {
             Session::put('UserSystem'   ,Auth::user()->username);
         }
 
-
-        if (!Session::has('UserPassword')) {
-            Session::put('UserPassword'   ,Auth::user()->change_password);
-        }
-
+        $Users = User::select('change_password')->where('id','=',Auth::user()->id )->get()->toArray();
+        Session::put('UserPassword'     ,$Users[0]['change_password']);
 
         $Anexos = Anexo::select('name')->where('user_id','=',Auth::user()->id )->get()->toArray();
         if(count($Anexos) != 0){
             $name_anexo = $Anexos[0]['name'];
-            Session::put('UserAnexo'   ,$name_anexo);
+            Session::put('UserAnexo'    ,$name_anexo);
         }else{
-            Session::put('UserAnexo'   ,'Sin Anexo'   );
+            Session::put('UserAnexo'     ,'Sin Anexo'   );
         }
 
         $this->UserId       = Session::get('UserId')        ;
@@ -56,7 +54,6 @@ class AdminController extends Controller
         $this->UserSystem   = Session::get('UserSystem')    ;
         $this->UserPassword = Session::get('UserPassword')  ;
         $this->UserAnexo    = Session::get('UserAnexo')     ;
-
 
     }
 
