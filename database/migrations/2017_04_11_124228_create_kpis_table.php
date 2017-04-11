56b5f8c6d329ce913f3468e87c3a987920330ae4
Raúl Dominguez <jdominguezr93@gmail.com>
2017-04-11 12:53:43 -0500

    Cree las tablas agent_online y kpis para la nueva estructura de la base de datos.

---
+++ database/migrations/2017_04_11_124228_create_kpis_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKpisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kpis', function (Blueprint $table){
+
+            $table->increments('id');
+            $table->string('name',50);
+            $table->string('text',50);
+            $table->string('symbol',5);
+            $table->integer('time',5);
+            $table->string('color',50);
+            $table->string('icon',50);
+            $table->integer('estado_id',10)->unsigned();
+
+            // creando relaciones:
+            $table->foreign('estado_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('kpis');
+    }
+}
