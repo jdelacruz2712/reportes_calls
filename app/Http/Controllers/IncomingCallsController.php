15c8c401bfa453260501c8ee6a0e1670f77bf0c3
Alan Cornejo <alancornejo@icloud.com>
2017-07-18 17:31:02 -0500

    Controlar Evento BLINDTRANSFER, para mostrar datos en el reporte Incoming

--- app/Http/Controllers/IncomingCallsController.php
+++ app/Http/Controllers/IncomingCallsController.php
@@ -105,10 +105,10 @@ class IncomingCallsController extends CosapiController
 
         switch($events){
             case 'calls_completed' :
-                $events             = array ('COMPLETECALLER', 'COMPLETEAGENT', 'TRANSFER');
+                $events             = array ('COMPLETECALLER', 'COMPLETEAGENT', 'TRANSFER', 'BLINDTRANSFER');
             break;
             case 'calls_transfer' :
-                $events             = array ('TRANSFER');
+                $events             = array ('TRANSFER', 'BLINDTRANSFER');
             break;
             case 'calls_abandone' :
                 $events             = array ('ABANDON');
@@ -137,6 +137,9 @@ class IncomingCallsController extends CosapiController
 
             switch ($query_call['event']) {
                 case 'TRANSFER':
+                    $action = 'Transferido a '.$query_call['url'];
+                    break;
+                case 'BLINDTRANSFER':
                     $action = 'Transferido a '.$query_call['url'];
                     break;
                 case 'ABANDON':
