19d34588cda5053aa30f7dceb1109f092e8e1f53
Alan Cornejo <alancornejo@icloud.com>
2017-07-24 12:37:14 -0500

    Cambios en los permisos para el rol BackOffice el cual no debe poder asignar colas ni ver la lista de usuarios.

--- resources/views/layout/recursos/left_colum.blade.php
+++ resources/views/layout/recursos/left_colum.blade.php
@@ -22,7 +22,7 @@
     <ul class="sidebar-menu">
         <li class="header">MENU PRINCIPAL</li>
 
-      @if($role != 'user')
+      @if($role != 'user' and  $role != 'backoffice')
         <li class="active treeview">
           <a href="#">
             <i class="fa fa-dashboard"></i> <span>DASHBOARD</span> <i class="fa fa-angle-left pull-right"></i>
@@ -53,7 +53,7 @@
             <i class="fa fa-phone-square"></i> <span>REPORTING CALLS</span> <i class="fa fa-angle-left pull-right"></i>
           </a>
           <ul class="treeview-menu">
-            @if($role != 'user')
+            @if($role != 'user' and  $role != 'backoffice')
               <li ><a tabindex="-1" href="#" id="agents_online" class="reportes"><i class="fa fa-circle-o text-red"></i> Agents Online</a></li>
             @endif
               <li><a href="#" id="incoming_calls"     class="reportes"><i class="fa fa-circle-o text-green"></i> Inbound Calls</a></li>
@@ -62,35 +62,33 @@
           </ul>
         </li>
 
-      @if($role != 'cliente')
-        <li>
-          <a href="#">
-            <i class="fa fa-book"></i> <span>REPORTING SURVEYS</span> <i class="fa fa-angle-left pull-right"></i>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#" id="surveys"              class="reportes"><i class="fa fa-circle-o text-purple"></i> Detail Surveys</a></li>
-          </ul>
-        </li>
+      @if($role != 'cliente' )
+            <li>
+                <a href="#">
+                    <i class="fa fa-book"></i> <span>REPORTING SURVEYS</span> <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="#" id="surveys"              class="reportes"><i class="fa fa-circle-o text-purple"></i> Detail Surveys</a></li>
+                </ul>
+            </li>
       @endif
 
-      @if($role != 'cliente')
-            @if($role != 'calidad')
-                <li>
-                  <a href="#">
+      @if($role != 'cliente' and $role != 'calidad')
+            <li>
+                <a href="#">
                     <i class="fa fa-gears"></i> <span>ADMINISTRATOR</span> <i class="fa fa-angle-left pull-right"></i>
-                  </a>
-                  <ul class="treeview-menu">
+                </a>
+                <ul class="treeview-menu">
                     <li><a href="#" id="agents_annexed"       class="reportes"><i class="fa fa-circle-o text-purple"></i> Assign Annexed</a></li>
-                    @if($role != 'user')
-                      <li><a href="#" id="list_users"         class="reportes"><i class="fa fa-circle-o text-red"></i> List Users</a></li>
-                      <li><a href="#" id="agents_queue"       class="reportes"><i class="fa fa-circle-o text-green"></i> Assign Queue</a></li>
+                    @if($role != 'user' and  $role != 'backoffice')
+                        <li><a href="#" id="list_users"         class="reportes"><i class="fa fa-circle-o text-red"></i> List Users</a></li>
+                        <li><a href="#" id="agents_queue"       class="reportes"><i class="fa fa-circle-o text-green"></i> Assign Queue</a></li>
                     @endif
                     @if(Session::get('ChangeRole') == 1)
-                      <li><a href="#" id="activate_calls"     class="activate_calls" onclick="activeCalls()"><i class="fa fa-circle-o text-orange"></i> Activate Calls</a></li>
+                        <li><a href="#" id="activate_calls"     class="activate_calls" onclick="activeCalls()"><i class="fa fa-circle-o text-orange"></i> Activate Calls</a></li>
                     @endif
-                  </ul>
-                </li>
-            @endif
+                </ul>
+            </li>
       @endif
 
     </ul>
