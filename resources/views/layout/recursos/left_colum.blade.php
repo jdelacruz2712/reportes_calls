c4829abc09018ad0880de086e020d49a8a0c7b90
Jeancarlos De la Cruz Criollo <jdelacruz@sapia.com.pe>
2017-07-25 13:56:29 -0500

    Se corrigue los modulos que pueden ver los siguientes roles : - Calidad - Backoffice - Cliente

--- resources/views/layout/recursos/left_colum.blade.php
+++ resources/views/layout/recursos/left_colum.blade.php
@@ -33,20 +33,20 @@
         </li>
       @endif
 
-      @if($role != 'cliente')
         <li>
           <a href="#">
             <i class="fa fa-calendar-check-o"></i> <span>REPORTING EVENTS</span> <i class="fa fa-angle-left pull-right"></i>
           </a>
           <ul class="treeview-menu">
-            <li><a href="#" id="events_detail" class="reportes"><i class="fa fa-circle-o text-purple"></i> Detail Events</a></li>
+            @if($role != 'cliente')
+              <li><a href="#" id="events_detail" class="reportes"><i class="fa fa-circle-o text-purple"></i> Detail Events</a></li>
+            @endif
             @if($role != 'user')
               <li><a href="#" id="events_consolidated"  class="reportes"><i class="fa fa-circle-o text-yellow"></i> Consolidated Events</a></li>
               <li><a href="#" id="level_of_occupation"  class="reportes"><i class="fa fa-circle-o text-red"></i> Level Of Occupation</a></li>
             @endif
           </ul>
         </li>
-      @endif
 
         <li>
           <a href="#">
@@ -74,23 +74,25 @@
       @endif
 
       @if($role != 'cliente')
+        <li>
+          <a href="#">
+            <i class="fa fa-gears"></i> <span>ADMINISTRATOR</span> <i class="fa fa-angle-left pull-right"></i>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#" id="agents_annexed"       class="reportes"><i class="fa fa-circle-o text-purple"></i> Assign Annexed</a></li>
             @if($role != 'calidad')
-                <li>
-                  <a href="#">
-                    <i class="fa fa-gears"></i> <span>ADMINISTRATOR</span> <i class="fa fa-angle-left pull-right"></i>
-                  </a>
-                  <ul class="treeview-menu">
-                    <li><a href="#" id="agents_annexed"       class="reportes"><i class="fa fa-circle-o text-purple"></i> Assign Annexed</a></li>
-                    @if($role != 'user')
-                      <li><a href="#" id="list_users"         class="reportes"><i class="fa fa-circle-o text-red"></i> List Users</a></li>
-                      <li><a href="#" id="agents_queue"       class="reportes"><i class="fa fa-circle-o text-green"></i> Assign Queue</a></li>
-                    @endif
-                    @if(Session::get('ChangeRole') == 1)
-                      <li><a href="#" id="activate_calls"     class="activate_calls" onclick="activeCalls()"><i class="fa fa-circle-o text-orange"></i> Activate Calls</a></li>
-                    @endif
-                  </ul>
-                </li>
+              @if($role != 'backoffice')
+                @if($role != 'user')
+                  <li><a href="#" id="list_users"         class="reportes"><i class="fa fa-circle-o text-red"></i> List Users</a></li>
+                  <li><a href="#" id="agents_queue"       class="reportes"><i class="fa fa-circle-o text-green"></i> Assign Queue</a></li>
+                @endif
+              @endif
+              @if(Session::get('ChangeRole') == 1)
+                <li><a href="#" id="activate_calls"     class="activate_calls" onclick="activeCalls()"><i class="fa fa-circle-o text-orange"></i> Activate Calls</a></li>
+              @endif
             @endif
+          </ul>
+        </li>
       @endif
 
     </ul>
