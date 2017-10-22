92ae224a4ba8080f9c1960830a4c084170bc7d26
Alan Cornejo <alancornejo@icloud.com>
2017-10-21 21:37:23 -0500

    Solucionar nombre de rol supervisor.

--- resources/views/layout/recursos/left_menu/index.blade.php
+++ resources/views/layout/recursos/left_menu/index.blade.php
@@ -5,10 +5,10 @@
     <ul class="sidebar-menu" data-widget="tree" id="menuleft">
       <li class="header">Menu </li>
 
-      <template v-if="getRole === 'admin' || getRole === 'supervision'">
+      <template v-if="getRole === 'admin' || getRole === 'supervisor'">
         @include('layout.recursos.left_menu.supervision')
       </template>
-      
+
       <template v-if="getRole === 'user' ">  @include('layout.recursos.left_menu.user') </template>
       <template v-if="getRole === 'backoffice' ">  @include('layout.recursos.left_menu.backoffice') </template>
 
