01661c48e4e6cd03288d547866dced7c07bb80ba
Alan Cornejo <alancornejo@icloud.com>
2017-10-21 21:58:47 -0500

    Controlar bug de rol Monitor.

--- resources/views/layout/recursos/left_menu/index.blade.php
+++ resources/views/layout/recursos/left_menu/index.blade.php
@@ -6,12 +6,12 @@
       <li class="header">Menu </li>
 
       <template v-if="getRole === 'admin' || getRole === 'supervisor'">
-        @include('layout.recursos.left_menu.supervision')
+        <template v-if="getUsername === 'monitor' ">  @include('layout.recursos.left_menu.monitor') </template>
+        <template v-else>  @include('layout.recursos.left_menu.supervision') </template>
       </template>
 
       <template v-if="getRole === 'user' ">  @include('layout.recursos.left_menu.user') </template>
       <template v-if="getRole === 'backoffice' ">  @include('layout.recursos.left_menu.backoffice') </template>
-      <template v-if="getUsername === 'monitor' ">  @include('layout.recursos.left_menu.monitor') </template>
     </ul>
   </section>
 </aside>
