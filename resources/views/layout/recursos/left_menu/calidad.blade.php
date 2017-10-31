9f06608b60aa77557610b9a65acb8d2f9c8feff1
Alan Cornejo <alancornejo@icloud.com>
2017-10-31 11:57:15 -0500

    Mostrar el menu para el perfil calidad. Cambiar nombre de report detail de eventos.

--- resources/views/layout/recursos/left_menu/calidad.blade.php
+++ resources/views/layout/recursos/left_menu/calidad.blade.php
@@ -1,11 +1,20 @@
 <li class="treeview">
     <a href="javascript:void(0)">
+        <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
+    </a>
+    <ul class="treeview-menu">
+        <li><a href="dashboard_01" target="_blank"><i class="fa fa-circle-o text-red"></i> Dashboard 01</a></li>
+        <!--<li><a href="#" v-on:click="loadOptionMenu('dashboard_03')"> <i class="fa fa-circle-o text-blue"></i> Dashboard 03</a></li>-->
+    </ul>
+</li>
+
+<li class="treeview">
+    <a href="javascript:void(0)">
         <i class="fa fa-calendar-check-o"></i> <span>Reporting Events</span> <i class="fa fa-angle-left pull-right"></i>
     </a>
     <ul class="treeview-menu">
         <li><a href="#" v-on:click="loadOptionMenu('events_detail')" class="reportes"><i class="fa fa-circle-o text-purple"></i> Detail Events</a></li>
         <li><a href="#" v-on:click="loadOptionMenu('events_consolidated')"  class="reportes"><i class="fa fa-circle-o text-yellow"></i> Consolidated Events</a></li>
-        <li><a href="#" v-on:click="loadOptionMenu('detail_event_report')"  class="reportes"><i class="fa fa-circle-o text-fuchsia"></i> Level Of Occupation</a></li>
     </ul>
 </li>
 
@@ -36,5 +45,6 @@
     </a>
     <ul class="treeview-menu">
         <li><a href="#" v-on:click="loadOptionMenu('agents_annexed')"> <i class="fa fa-circle-o text-purple"></i> Assign Annexed</a></li>
+        <!--<li><a href="#" v-on:click="loadOptionMenu('broadcast_message')"> <i class="fa fa-circle-o text-green"></i> BroadCast Message</a></li>-->
     </ul>
 </li>
--- resources/views/layout/recursos/left_menu/index.blade.php
+++ resources/views/layout/recursos/left_menu/index.blade.php
@@ -12,6 +12,7 @@
 
       <template v-if="getRole === 'user' ">  @include('layout.recursos.left_menu.user') </template>
       <template v-if="getRole === 'backoffice' ">  @include('layout.recursos.left_menu.backoffice') </template>
+      <template v-if="getRole === 'calidad' ">  @include('layout.recursos.left_menu.calidad') </template>
     </ul>
   </section>
 </aside>
